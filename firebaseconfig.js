78b18f3eeed16645a73f8999cddf135353ffb047
Osama M.Younus <osamayounus541@gmail.com>
2022-11-12 10:29:30 +0500

    admin panel done

---
+++ firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDb5A5Ab4KqWKqNn9vLuwxmCO5pDKRkx00",
+  authDomain: "mini-hackathon-11294.firebaseapp.com",
+  projectId: "mini-hackathon-11294",
+  storageBucket: "mini-hackathon-11294.appspot.com",
+  messagingSenderId: "498950641472",
+  appId: "1:498950641472:web:b57c9ebeee2d7698c00ca5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
