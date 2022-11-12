de0755df8e240c5c50b4199c31dfc5a1009cdb6b
Osama M.Younus <osamayounus541@gmail.com>
2022-11-12 14:51:41 +0500

    data added to firebase

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
+import {app} from "./firebaseconfig.js"
+import {db} from "./firebaseconfig.js"
+import { collection, addDoc,getDocs, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"; 
+
 let clstime = document.querySelector("#clstime");
 let clsschedule = document.querySelector("#clsschedule");
 let tchName = document.querySelector("#tchName");
 let secName = document.querySelector("#secName");
-let courseName = document.querySelector("#clstime");
+let courseName = document.querySelector("#courseName");
 let batchNum = document.querySelector("#batchNum");
+let name = document.querySelector("#name");
+let fName = document.querySelector("#fname");
+let rollNum = document.querySelector("#rollNum");
+let cntcNum = document.querySelector("#cntctNum");
+let cnicNum = document.querySelector("#cnicNum");
+let stCourseName = document.querySelector("#stCourseName");
+let pic = document.querySelector("#pic");
 let clsBtn = document.querySelector("#clsBtn");
+
 
 clsBtn.addEventListener("click",classButton);
 
-function classButton(){
-    console.log(clstime.value);
-    console.log(clsschedule.value);
-    console.log(tchName.value);
-    console.log(secName.value);
-    console.log(courseName.value);
-    console.log(batchNum.value);
+async function classButton(){
+ 
+const classRef = collection(db, "Class");
+
+await setDoc(doc(classRef, "Sir Haider"), {
+    ClassTime: clstime.value,
+          ClassSchedule: clsschedule.value,
+           TeacherName:tchName.value,
+         SectionName: secName.value,
+           CourseName: courseName.value,
+      BatchNumber: batchNum.value, 
+      Name: name.value,
+        FatherName: fName.value,
+        RollNum: rollNum.value,
+        ContactNum: cntcNum.value,
+        CNIC: cnicNum.value,
+        StudentCourse: stCourseName.value,
+        Picture: pic.value
+    });
+
+      
 }
--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getFirestore} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyDb5A5Ab4KqWKqNn9vLuwxmCO5pDKRkx00",
   authDomain: "mini-hackathon-11294.firebaseapp.com",
@@ -15,3 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
