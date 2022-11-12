9b7cd96acc45ad1c409bd11e9c05458fdfa8a191
Osama M.Younus <osamayounus541@gmail.com>
2022-11-12 11:52:29 +0500

    class section ui compl and and get data

---
+++ app.js
@@ -0,0 +1,18 @@
+let clstime = document.querySelector("#clstime");
+let clsschedule = document.querySelector("#clsschedule");
+let tchName = document.querySelector("#tchName");
+let secName = document.querySelector("#secName");
+let courseName = document.querySelector("#clstime");
+let batchNum = document.querySelector("#batchNum");
+let clsBtn = document.querySelector("#clsBtn");
+
+clsBtn.addEventListener("click",classButton);
+
+function classButton(){
+    console.log(clstime.value);
+    console.log(clsschedule.value);
+    console.log(tchName.value);
+    console.log(secName.value);
+    console.log(courseName.value);
+    console.log(batchNum.value);
+}
